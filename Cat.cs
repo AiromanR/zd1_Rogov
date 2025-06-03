8d86ee641ecc18b2f2d1415ef1fd351b2f75a4e8
AiromanR <110393090+AiromanR@users.noreply.github.com>
2025-06-03 22:51:04 +0500

    Add files via upload

---
+++ Cat.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zd1_Rogov
+{
+    internal class Cat
+    {
+        private string name;
+        public string Name
+        {
+            get{ return name; }
+            set
+            {
+                bool OnlyLetters = true; //проверка на наличие букв
+                foreach (var ch in value)
+                {
+                    if (!char.IsLetter(ch))
+                    {
+                        OnlyLetters = false;
+                    }
+                }
+                if (OnlyLetters)
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine($"{value} - неправильное имя!!!");
+                }
+            }
+        }
+        private double weight;
+        public double Weight // Установка свойства вес с проверками
+        {
+            get { return weight; }
+            set
+            {
+                if (value == 0) 
+                {
+                    Console.WriteLine("Вес не должен быть нулевым");
+                }
+                else if (value < 0)
+                {
+                    Console.WriteLine("Вес не должен быть отрицательным");
+                }
+                else
+                {
+                    weight = value;
+                }
+            }
+        }
+        public Cat(string CatName)
+        {
+            Name = CatName;
+        }
+        public void Meow() //добавил отображение веса
+        {
+            Console.WriteLine($"{name}: МЯЯЯЯУ!!!! Вес: {weight}");
+        }
+
+        //public void SetCatName(string CatName)
+        //{
+
+        //    bool OnlyLetters = true;
+
+        //    foreach (var ch in CatName)
+        //    {
+        //        if (!char.IsLetter(ch))
+        //        {
+        //            OnlyLetters = false;
+        //        }
+        //    }
+
+        //    if (OnlyLetters)
+        //        name = CatName;
+        //    else
+        //        Console.WriteLine($"{CatName} - неправильное имя!!!");
+        //}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using zd1_Rogov;
+
+Console.WriteLine("Введите имя первого кота");
+Cat cat1 = new Cat(Console.ReadLine());
+Console.WriteLine("Введите вес первого кота");
+cat1.Weight = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите имя второго кота");
+Cat cat2 = new Cat(Console.ReadLine());
+Console.WriteLine("Введите вес второго кота");
+cat2.Weight = Convert.ToDouble(Console.ReadLine());
+
+cat1.Meow();
+cat2.Meow();
+
+Console.WriteLine("Измените имя первого кота");
+cat1.Name = Console.ReadLine();
+cat1.Meow();
+
+Console.WriteLine("Измените вес второго кота");
+cat2.Weight = Convert.ToDouble(Console.ReadLine());
+cat2.Meow();
+
